be7af64f76ce78ae69fa420f791f416dd054f5f8
ceybers <craig.eybers@gmail.com>
2024-03-18 14:56:31 +0200

    VIAL/QMK Firmware

---
+++ firmware/cae86/qazortho38/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+#define VIAL_KEYBOARD_UID {0x0C, 0x0E, 0x08, 0x06, 0x00, 0x00, 0x00, 0x05}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 9 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
